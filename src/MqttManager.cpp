912d4891c64e4ef13be76d43ca9ef9ce876abc25
NICOLAS MARCELINO LOPES <54815787867@info.local>
2026-04-28 17:18:13 -0300

    Criado o cabeçalho do MQTTManager e as funções do Debug

--- src/MqttManager.cpp
+++ src/MqttManager.cpp
@@ -1,4 +1,9 @@
-//!MqttManager.cpp
+//* MqttManager.cpp
 #include <Arduino.h>
-#include "MqttManager.h"
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+
+#include "secrets.h"
+#include "WiFiManager.h"
+#include "MqttManager.h"
--- src/WiFiManager.cpp
+++ src/WiFiManager.cpp
@@ -1,68 +1,69 @@
-//! WiFiManager.cpp
+//* WiFiManager.cpp
 #include <Arduino.h>
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
 #include "WiFiManager.h"
 #include "secrets.h"
-
-bool wifiEstaConectado ()
-{
-    return WiFi.status() == WL_CONNECTED;
-}
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFi.h>
+#include "DebugManager.h"
 
 void conectarWiFi()
 {
-  Serial.println("===========================");
-  Serial.println("Iniciando conexão WiFi...");
-  Serial.println("===========================");
-  
-  //Configura o ESP32 como station, ou seja
-  //ele vai se conectar a um roteador existente
-  WiFi.mode(WIFI_STA);         
-  
-  //Inicia a conexão com SSID e senha
+  debugInfo("===========================");
+  debugInfo("Iniciando conexão WiFi...");
+  debugInfo("===========================");
+
+  // Configura o ESP32 como station, ou seja
+  // ele vai se conectar a um roteador existente
+  WiFi.mode(WIFI_STA);
+
+  // Inicia a conexão com SSID e senha
   WiFi.begin(WIFI_SSID, WIFI_SENHA);
 
-  Serial.println("Conectando");
+  debugInfo("Conectando");
 
   int tentativasWiFi = 0;
   const int maxTentativasWiFi = 30;
 
-  //Aguarda a conexão por até 30 tentativas
-  while(WiFi.status() != WL_CONNECTED && tentativasWiFi < maxTentativasWiFi)
+  // Aguarda a conexão por até 30 tentativas
+  while (WiFi.status() != WL_CONNECTED && tentativasWiFi < maxTentativasWiFi)
   {
     delay(500);
-    Serial.print(".");
+    debugInfoSemLinha(".");
     tentativasWiFi++;
   }
 
-  Serial.println();
+  debugInfo("");
 
-  if(WiFi.status() == WL_CONNECTED)
+  if (WiFi.status() == WL_CONNECTED)
   {
-    Serial.println("WiFi connectado com sucesso!");
-    Serial.print("Endereço IP: ");
-    Serial.println(WiFi.localIP());
+    debugInfo("WiFi connectado com sucesso!");
+    debugInfo("Endereço IP: ");
+    debugInfo(String( WiFi.localIP() ) );
   }
 
   else
   {
-    Serial.println("Falha ao conectar no WiFi.");
-    Serial.println("Verifique SSID, senha e sinal de rede.");
+    debugErro("Falha ao conectar no WiFi.");
+    debugErro("Verifique SSID, senha e sinal de rede.");
   }
 }
 
-
 void garantirWiFiConectado()
 {
-  if(WiFi.status() != WL_CONNECTED)
+  if (WiFi.status() != WL_CONNECTED)
   {
-    Serial.println("WiFi desconectado. Tentando reconectar...");
+    debugErro("WiFi desconectado. Tentando reconectar...");
     conectarWiFi();
   }
 
-  if(WiFi.status() != WL_CONNECTED)
+  if (WiFi.status() != WL_CONNECTED)
   {
-    Serial.println("Não foi possível reconectar ao WiFi.");
+    debugErro("Não foi possível reconectar ao WiFi.");
   }
+}
+
+bool wifiEstaConectado()
+{
+  return WiFi.status() == WL_CONNECTED;
 }
