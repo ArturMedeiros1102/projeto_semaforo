58c3e4e6603f320f90782288c56afb4c4550de65
NICOLAS MARCELINO LOPES <54815787867@info.local>
2026-04-29 17:20:00 -0300

    Iniciado Arquivo Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,28 @@
-
 #include <Arduino.h>
+
 #include "WiFiManager.h"
+#include "MqttManager.h"
+#include "DebugManager.h"
+
+void tratarMensagemRecebida(const char* topico, const String& mensagem);
 
 void setup()
 {
-  Serial.begin(9600);
+  configurarDebug();
   conectarWiFi();
+  configurarMQTT();
+  registrarCallbackMensagem(tratarMensagemRecebida);
+  conectarMQTT();
 }
 
 void loop()
 {
   garantirWiFiConectado();
+  garantirMQTTConectado();
+  loopMQTT();
+}
+
+void tratarMensagemRecebida(const char* topico, const String& mensagem)
+{
+
 }
